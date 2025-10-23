4ab4db9c58aa030d32f559912a4612677aa269f6
wawan <wawampras@gmail.com>
2025-10-23 20:12:10 +0700

    add test case register

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,10 +7,33 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add('visitUrl', () => {
+    cy.visit('https://automationteststore.com/')
+})
+Cypress.Commands.add('registerAccount', (data) => {
+    cy.xpath("//a[.='Login or register']").click()
+    cy.xpath("//button[contains(.,'Continue')]").click()
+    cy.get('#AccountFrm_firstname').type(data.firstName)
+    cy.get('#AccountFrm_lastname').type(data.lastName)
+    cy.get('#AccountFrm_email').type(data.email)
+    cy.get('#AccountFrm_telephone').type(data.telephone)
+    cy.get('#AccountFrm_fax').type(data.fax)
+    cy.get('#AccountFrm_company').type(data.company)
+    cy.get('#AccountFrm_country_id').select(data.country)
+    cy.get('#AccountFrm_zone_id').select(data.region)
+    cy.get('#AccountFrm_address_1').type(data.address1)
+    cy.get('#AccountFrm_address_2').type(data.address2)
+    cy.get('#AccountFrm_city').type(data.city)
+    cy.get('#AccountFrm_postcode').type(data.postCode)
+    cy.get('#AccountFrm_loginname').type(data.loginName)
+    cy.get('#AccountFrm_password').type(data.loginPassword)
+    cy.get('#AccountFrm_confirm').type(data.loginPasswordConfirm)
+    cy.get("#AccountFrm_newsletter1").click()
+    cy.get("#AccountFrm_agree").click()
+    cy.xpath("//button[@class='btn btn-orange pull-right lock-on-click']").click()
+})
 //
 //
 // -- This is a child command --
